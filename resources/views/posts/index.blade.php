89e5833480b54f8a9806e79de05fafbb5af07349
Danniel Libor <dlibor.dev@gmail.com>
2025-10-26 10:16:29 +0800

    feat: post create

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,27 +1,46 @@
 <x-layouts.app :title="__('Posts')">
-    <flux:brand href="#" logo="/img/demo/logo.png" name="Acme Inc." />
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+            <h2 class="text-4xl font-semibold tracking-tight text-pretty text-white sm:text-5xl">From the blog</h2>
+            <p class="mt-2 text-lg/8 text-gray-300">Learn how to grow your business with our expert advice.</p>
 
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div
-                class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern
-                    class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div
-                class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern
-                    class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div
-                class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern
-                    class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
+            <flux:button href="{{ route('posts.create') }}" variant="primary">
+                Create new post
+            </flux:button>
         </div>
         <div
-            class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-700 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <article class="flex max-w-xl flex-col items-start justify-between">
+                <div class="flex items-center gap-x-4 text-xs">
+                    <time datetime="2020-03-16" class="text-gray-400">Mar 16, 2020</time>
+                    <a href="#"
+                        class="relative z-10 rounded-full bg-gray-800/60 px-3 py-1.5 font-medium text-gray-300 hover:bg-gray-800">Marketing</a>
+                </div>
+                <div class="group relative grow">
+                    <h3 class="mt-3 text-lg/6 font-semibold text-white group-hover:text-gray-300">
+                        <a href="#">
+                            <span class="absolute inset-0"></span>
+                            Boost your conversion rate
+                        </a>
+                    </h3>
+                    <p class="mt-5 line-clamp-3 text-sm/6 text-gray-400">Illo sint voluptas. Error voluptates culpa
+                        eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed
+                        exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                </div>
+                <div class="relative mt-8 flex items-center gap-x-4 justify-self-end">
+                    <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt="" class="size-10 rounded-full bg-gray-800" />
+                    <div class="text-sm/6">
+                        <p class="font-semibold text-white">
+                            <a href="#">
+                                <span class="absolute inset-0"></span>
+                                Michael Foster
+                            </a>
+                        </p>
+                        <p class="text-gray-400">Co-Founder / CTO</p>
+                    </div>
+                </div>
+            </article>
         </div>
     </div>
 </x-layouts.app>
